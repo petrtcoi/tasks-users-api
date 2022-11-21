511c1cfb8d49f4293b20b87b7adfbdb9c8041aa2
Petr Tcoi <petrtcoi@outlook.com>
2022-11-21 10:52:16 +0300

    add helmet

--- dist/app.js
+++ dist/app.js
@@ -7,6 +7,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const helmet_1 = __importDefault(require("helmet"));
 const env_config_1 = require("./env.config");
 require('express-async-errors');
 const router_1 = __importDefault(require("./endpoints/tasks/router"));
@@ -18,7 +19,7 @@ Object.freeze(Object.prototype);
 const app = (0, express_1.default)();
 // MIDDLEWARE
 // app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
-app.use(express_1.default.static('public'));
+app.use((0, helmet_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cookie_parser_1.default)());
--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ const app: Express = express()
 
 // MIDDLEWARE
 // app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
-app.use(express.static('public'))
+app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
